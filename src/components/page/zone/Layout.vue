20231d31eaa60655d83712706909104b4cd5fefc
jackbuy <2538362801@qq.com>
2019-10-21 20:13:10 +0800

    update

---
+++ src/components/page/zone/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="zone">
+        <div class="zone__header">
+            <slot name="header"></slot>
+        </div>
+        <div class="zone__content">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LayOut',
+    props: {}
+};
+</script>

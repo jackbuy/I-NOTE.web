3216f2c6a3963e8e20ec7d447970031593b0031d
jackbuy <2538362801@qq.com>
2019-10-31 17:14:33 +0800

    update

--- src/constants/url-config.js
+++ src/constants/url-config.js
@@ -1,8 +1,8 @@
 // 访问图片地址
 // export const imgBaseUrl = 'http://localhost:8081/resouces/';
-// export const imgBaseUrl = 'http://119.23.10.4:10000';
-export const imgBaseUrl = 'http://localhost:10000';
+export const imgBaseUrl = 'http://119.23.10.4:10000';
+// export const imgBaseUrl = 'http://localhost:10000';
 
 // api地址
-// export const apiBaseUrl = 'http://119.23.10.4:10000';
-export const apiBaseUrl = 'http://localhost:10000';
+export const apiBaseUrl = 'http://119.23.10.4:10000';
+// export const apiBaseUrl = 'http://localhost:10000';
